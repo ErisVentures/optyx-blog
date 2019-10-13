7ce57902b1bf64182d1da9ac5ee569809e3303ef
Patrick Hulce <patrick.hulce@gmail.com>
2019-10-13 17:22:50 -0500

    refactor: rename product to tutorial

--- config.rb
+++ config.rb
@@ -37,12 +37,12 @@ end
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy product.yml files to product.html 
-data.products.each do |_filename, product|
-  # product is an array: [filename, {data}]
-  proxy "/product/#{product[:title].parameterize}/index.html", "product.html", 
-  locals: {product: product}, 
-  layout: 'product-detail',
+# proxy tutorial.yml files to tutorial.html
+data.tutorials.each do |_filename, tutorial|
+  # tutorial is an array: [filename, {data}]
+  proxy "/tutorials/#{tutorial[:title].parameterize}/index.html", "tutorial.html",
+  locals: {tutorial: tutorial},
+  layout: 'tutorial-detail',
   ignore: true
 end
 
@@ -58,7 +58,7 @@ helpers do
   def background_image(image)
     "background-image: url('" << image_path(image) << "')"
   end
-  
+
   def nav_link(link_text, url, options = {})
     options[:class] ||= ""
     options[:class] << " active" if url == current_page.url
--- source/admin/previews.js
+++ source/admin/previews.js
@@ -1,4 +1,4 @@
-const BlogPreview = ({ entry, widgetFor }) => {
+const BlogPreview = ({entry, widgetFor}) => {
   const data = entry.get('data').toJS()
   const date = dayjs(data.date).format('DD-MM-YY')
   return (
@@ -12,7 +12,7 @@ const BlogPreview = ({ entry, widgetFor }) => {
         </div>
         <div className="row">
           <div className="image">
-            <img src={data.image}/>
+            <img src={data.image} />
           </div>
         </div>
         <div className="row content">{widgetFor('body')}</div>
@@ -21,11 +21,11 @@ const BlogPreview = ({ entry, widgetFor }) => {
   )
 }
 
-const ProductPreview = ({ entry, widgetFor }) => {
+const ProductPreview = ({entry, widgetFor}) => {
   const data = entry.get('data').toJS()
   return (
-    <div className="product-detail">
-      <div className="image" style={{ backgroundImage: `url('${data.image}')` }}></div>
+    <div className="tutorial-detail">
+      <div className="image" style={{backgroundImage: `url('${data.image}')`}}></div>
       <div className="content">
         <div className="meta">
           <h1>{data.title}</h1>
@@ -42,11 +42,11 @@ const ProductPreview = ({ entry, widgetFor }) => {
   )
 }
 
-const HomePreview = ({ entry, widgetFor }) => {
+const HomePreview = ({entry, widgetFor}) => {
   const data = entry.get('data').toJS()
   return (
     <div>
-      <section className="hero" style={{ backgroundImage: `url('${data.hero.image}')` }}>
+      <section className="hero" style={{backgroundImage: `url('${data.hero.image}')`}}>
         <div className="caption">
           <div className="row">
             <h1>{data.hero.title}</h1>
@@ -64,25 +64,27 @@ const HomePreview = ({ entry, widgetFor }) => {
   )
 }
 
-const NavigationPreview = ({ entry }) => {
+const NavigationPreview = ({entry}) => {
   const data = entry.get('data').toJS()
   return (
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.svg" alt="Kaldi"/>
+          <img src="/images/logo.svg" alt="Kaldi" />
         </a>
       </div>
       <a className="nav-button">
-        <img src="/images/nav-open.svg" className="open"/>
+        <img src="/images/nav-open.svg" className="open" />
       </a>
-      <nav className="navigation" style={{ paddingRight: '40px' }}>
+      <nav className="navigation" style={{paddingRight: '40px'}}>
         <ul>
-          {!data.nav_items ? null : data.nav_items.map((item, idx) => (
-            <li key={idx}>
-              <a href={item.path}>{item.label}</a>
-            </li>
-          ))}
+          {!data.nav_items
+            ? null
+            : data.nav_items.map((item, idx) => (
+                <li key={idx}>
+                  <a href={item.path}>{item.label}</a>
+                </li>
+              ))}
         </ul>
       </nav>
     </header>
@@ -90,6 +92,6 @@ const NavigationPreview = ({ entry }) => {
 }
 
 CMS.registerPreviewTemplate('blog', BlogPreview)
-CMS.registerPreviewTemplate('product', ProductPreview)
+CMS.registerPreviewTemplate('tutorial', ProductPreview)
 CMS.registerPreviewTemplate('home', HomePreview)
 CMS.registerPreviewTemplate('navigation', NavigationPreview)
