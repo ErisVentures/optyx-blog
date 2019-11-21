a2b7e6f1fe28a0cb95688038629a9ba73e4b5562
Patrick Hulce <patrick.hulce@gmail.com>
2019-11-21 16:33:42 -0600

    feat: larger hero home image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import {Link, graphql} from "gatsby"
-import Image from "gatsby-image"
+import React from 'react'
+import {Link, graphql} from 'gatsby'
+import Image from 'gatsby-image'
 
-import {PostList} from "../components/post-list"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import {PostList} from '../components/post-list'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 class BlogIndex extends React.Component {
   render() {
@@ -15,22 +15,22 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="Home" />
-        <div className="hero" style={{position: "relative"}}>
+        <div className="hero" style={{position: 'relative'}}>
           <Image
-            style={{height: "60vh", minHeight: 400}}
-            imgStyle={{height: "60vh", minHeight: 400, objectFit: "cover"}}
+            style={{height: '70vh', minHeight: 400}}
+            imgStyle={{height: '70vh', minHeight: 400, objectFit: 'cover'}}
             fluid={data.hero.childImageSharp.fluid}
           />
           <h1
             style={{
-              position: "absolute",
+              position: 'absolute',
               marginTop: 0,
               borderBottom: 0,
-              top: "50%",
+              top: '50%',
               left: 0,
               right: 0,
-              transform: "translateY(-50%",
-              padding: "calc(var(--base-spacing) * 2)",
+              transform: 'translateY(-50%',
+              padding: 'calc(var(--base-spacing) * 2)',
             }}
           >
             <div className="container">
@@ -41,14 +41,14 @@ class BlogIndex extends React.Component {
         <div
           className="story"
           style={{
-            width: "100%",
-            padding: "calc(var(--base-spacing) * 4)",
-            background: "var(--theme-color)",
-            color: "white",
+            width: '100%',
+            padding: 'calc(var(--base-spacing) * 4)',
+            background: 'var(--theme-color)',
+            color: 'white',
           }}
         >
           <div className="container">
-            <p style={{fontSize: 24, lineHeight: "40px"}}>
+            <p style={{fontSize: 24, lineHeight: '40px'}}>
               Optyx is the AI photo manager you always wished you had. Automatically categorize,
               rate, and cull photos with advanced artificial intelligence. All processing happens
               on-device and is metadata-compatible with your other tools like Lightroom.
