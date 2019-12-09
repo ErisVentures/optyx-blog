5cace4bd96ebd3556deef338f7ffc197aa6a2160
Patrick Hulce <patrick.hulce@gmail.com>
2019-12-09 17:45:24 -0600

    feat: add markdown links for sections

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,13 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `-100`,
+              removeAccents: true,
+            },
+          },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
